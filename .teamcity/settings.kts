5c6aaf4f62d8b329e910db3b94c4786f6a7febdb
tombuildsstuff <git@ibuildstuff.co.uk>
2020-04-22 13:47:31 +0200

    fixing imports

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,11 @@
 package _Self.buildTypes
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.golang
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 version = "2019.2"
 
