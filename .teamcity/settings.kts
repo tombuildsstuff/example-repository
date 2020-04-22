25046dcc583d60577c8c8d8b716f8ed55ffb6eab
tombuildsstuff <git@ibuildstuff.co.uk>
2020-04-22 13:34:45 +0200

    w

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,12 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.golang
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+version = "2019.2"
+
+project {
+    vcsRoot(ExampleRepository)
+    buildType(ExampleConfiguration)
+}
 
 object ExampleRepository : GitVcsRoot({
   name = "example-repository"
